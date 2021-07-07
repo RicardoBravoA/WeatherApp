2d78abb598aba2949326942933bea74cc622e47a
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-06 20:15:07 -0500

    feat: Add data controller

---
+++ WeatherApp/Data/Storage/DataController.swift
@@ -0,0 +1,62 @@
+//
+//  DataController.swift
+//  WeatherApp
+//
+//  Created by Ricardo Bravo on 6/07/21.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    let persistentContainer: NSPersistentContainer
+    static let shared = DataController(modelName: "Weather")
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName:String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func configureContext() {
+        viewContext.automaticallyMergesChangesFromParent = true
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self.autoSaveViewContext()
+            self.configureContext()
+            completion?()
+        }
+    }
+    
+    func save() {
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+    }
+    
+}
+
+extension DataController {
+    func autoSaveViewContext(interval:TimeInterval = 30) {
+        
+        guard interval > 0 else {
+            print("cannot set negative autosave interval")
+            return
+        }
+        
+        save()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+}
