491f7d817fc75145f3622109bd1a25cc5c5019c8
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-07 04:06:44 -0500

    refactor: Remove cancel action

--- WeatherApp/Controller/AddLocationViewController/AddLocationViewController.swift
+++ WeatherApp/Controller/AddLocationViewController/AddLocationViewController.swift
@@ -29,10 +29,6 @@ class AddLocationViewController: UIViewController {
         performSegue(withIdentifier: "SearchSegue", sender: nil)
     }
     
-    @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "SearchSegue" {
             guard let searchViewController = segue.destination as? SearchViewController else {
