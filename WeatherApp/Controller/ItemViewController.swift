d126cfaf069b35e75beda964ecced598c3d5ec8e
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-07 01:35:41 -0500

    feat: Add Coord model

--- WeatherApp/Controller/ItemViewController.swift
+++ WeatherApp/Controller/ItemViewController.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class ItemViewController: UIViewController {
     
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherDescription: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var todayLabel: UILabel!
+    @IBOutlet weak var infoCollectionView: UICollectionView!
+    @IBOutlet weak var infoTableView: UITableView!
+    
+    static let identifier = "ItemViewController"
+    var location : Location!
+    var index = 0
+    
 }
---
+++ WeatherApp/Data/Model/City.swift
@@ -0,0 +1,19 @@
+//
+//  City.swift
+//  WeatherApp
+//
+//  Created by Ricardo Bravo on 7/07/21.
+//
+
+import Foundation
+
+struct City: Codable {
+    let id: Int
+    let name: String
+    let coord: Coord
+}
+
+struct Coord: Codable {
+    let lat: Double
+    let lon: Double
+}
